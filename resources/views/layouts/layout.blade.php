5c1889ce0b1656557d21c5eaf7fd5d214c9f78ce
messiJod <messi162108@gmail.com>
2022-06-01 17:45:52 +0530

    added HEART

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -13,7 +13,7 @@
     @yield('welcome')
     @yield('testimonial')
     @yield('detail')
-    <div class="absolute bottom-10 right-10 max-w-5xl text-white bg-gray-700 px-5 py-2">This is made with LARAVEL.</div>
+    <div class="absolute bottom-10 right-10 max-w-5xl text-white bg-gray-700 px-5 py-2">This is made with LARAVEL. &#128151;</div>
     <script src="{{asset('js/myscript.js')}}"></script>
 </body>
 </html>
